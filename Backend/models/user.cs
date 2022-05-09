172ef39621c3e21b448e62cdde543af547eb2965
Mcjos15 <63763240+Mcjos15@users.noreply.github.com>
2022-05-08 19:23:55 -0600

    mini update

---
+++ Backend/models/user.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Backend.models
+{
+    public class user
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private string correo { get; set; }
+        public user(int id, string name, string correo)
+        {
+            this.id = id;
+            this.name = name;
+            this.correo = correo;
+        }
+    }
+}
