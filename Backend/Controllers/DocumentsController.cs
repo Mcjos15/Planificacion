051758c652c4132fbf135fdcff9aa0c9a3087805
Mcjos15 <63763240+Mcjos15@users.noreply.github.com>
2022-05-29 23:25:56 -0600

    mini update documentos

--- Backend/Controllers/DocumentsController.cs
+++ Backend/Controllers/DocumentsController.cs
@@ -1,14 +1,46 @@
-﻿using System;
+﻿using Backend.Interfaces;
+using Backend.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
 
 namespace Backend.Controllers
 {
+    [Produces("application/json")]
+    [Route("api/[controller]")]
     public class DocumentsController : Controller
     {
-        
+        private readonly IDocument _iDocument;
+        public DocumentsController(IDocument iDocument)
+        {
+            _iDocument = iDocument;
+
+        }
+
+        [HttpGet]
+        public async Task<List<Document>> GetAll()
+        {
+
+            return await _iDocument.GetAllDocuments();
+
+        }
+        public async Task<IActionResult> Post([FromBody] Document document)
+        {
+
+            try
+            {
+                await _iDocument.AddDocument(document);
+
+                return CreatedAtAction(nameof(GetAll), new { id = document.id }, document);
+            }
+            catch (Exception)
+            {
+                return Problem("Problema al insertar"); ;
+            }
+
+        }
     }
 }
--- Backend/Services/DocumentService.cs
+++ Backend/Services/DocumentService.cs
@@ -1,10 +1,67 @@
-﻿using System;
+﻿using Backend.DataBase;
+using Backend.Interfaces;
+using Backend.models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
 namespace Backend.Services
 {
     public class DocumentService:IDocument
     {
-        public DocumentService()
+        private readonly IMongoCollection<Document> _documents;
+        public DocumentService(IOptions<Mongo> mongo)
         {
+            //context = new DBContextClass(mongo);
+
+            var mongoClient = new MongoClient(mongo.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(mongo.Value.DatabaseName);
+
+            // _users = mongoDatabase.GetCollection<Document>(mongo.Value.UsuariosCollectionName);
+            _documents = mongoDatabase.GetCollection<Document>("Documents");
+        }
+
+        public async Task AddDocument(Document item)=>
+            await _documents.InsertOneAsync(item);
+        
+
+    
+        public async Task<List<Document>> GetAllDocuments()
+        {
+            try
+            {
+                return await _documents.Find(_ => true).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                // log or manage the exception
+                throw ex;
+
+            }
+        }
+        public Task<Document> GetDocumentById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAllDocument()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveDocument(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDocument(string id, Document document)
+        {
+            throw new NotImplementedException();
         }
     }
 }
